96f6c3acbbf592a609fff3d265aa70001111e654
Laercio Lima Pilla <laercio.pilla@ufsc.br>
2016-10-13 18:02:42 -0300

    Three new distributions, internal interface has changed

--- src/distribution_generator.hpp
+++ src/distribution_generator.hpp
@@ -12,11 +12,13 @@ class UniformlySpaced;
 
 enum Generators {
     AVERAGE_POINT,
-    UNIFORMLY_SPACED
+    UNIFORMLY_SPACED,
+    EXPONENTIALLY_SPACED,
+    MID_POINT,
+    UNIFORMLY_RANDOM
 };
-
-// Example:
-// The class serves as a base and as a factory for specific implementations
+/****************************************************************************/
+// This class serves as a base and as a factory for specific implementations
 // of distributions
 class DistributionGenerator {
     protected:
@@ -28,15 +30,17 @@ class DistributionGenerator {
     public:
         //Factory method
         static DistributionGenerator *make_generator (
-            uint_fast64_t min, uint_fast64_t max, uint_fast64_t count_limit,
-            Generators generator_kind );
+            uint_fast64_t min, uint_fast64_t max, Generators generator_kind,
+            uint_fast64_t count_limit );
         //Returns true if the generation limit has been achieved
         bool is_done (  ) const { return ! (count_ < count_limit_); }
-        //Returns a point the in the distribution and increments the count
+        //Returns a point the in the distribution and increments the counter
         virtual uint_fast64_t next (  ) = 0;
 };
 
 // Distribution Generator that only provides the average point between limits
+// Example: AveragePoint(0,10) gives 5
+// [0] - 1 - 2 - 3 - 4 - (5) - 6 - 7 - 8 - 9 - [10]
 class AveragePoint : public DistributionGenerator {
     public:
         //Usual constructor with lower and upper limit
@@ -50,6 +54,11 @@ class AveragePoint : public DistributionGenerator {
         uint_fast64_t next (  );
 };
 
+// Distribution Generator that splits the interval in count_limit_ + 1 parts
+// Example: UniformlySpaced(0,10,1) gives 5
+// [0] - 1 - 2 - 3 - 4 - (5) - 6 - 7 - 8 - 9 - [10]
+// Example: UniformlySpaced(0,10,4) gives 2, 4, 6, and 8
+// [0] - 1 - (2) - 3 - (4) - 5 - (6) - 7 - (8) - 9 - [10]
 class UniformlySpaced : public DistributionGenerator {
     public:
         //Constructor with lower and upper limit and the number of parts to
@@ -59,20 +68,84 @@ class UniformlySpaced : public DistributionGenerator {
                 uint_fast64_t count_limit) {
             min_ = min;
             max_ = max;
-            count_limit_ = count_limit;
+            count_limit_ = count_limit + 1ul;
+            count_ = 1ul;
         }
-        //Provides the average point between lower and upper limits
+        //Provides the point splitting the next intervals
         uint_fast64_t next (  );
 };
 
+// Distribution Generator that splits the interval in count_limit + 1 parts.
+// Each part is exponentially larger than the one before
+// Example: ExponentiallySpaced(2,8,1) gives 4
+// [2] - 3 - (4) - 5 - 6 - 7 - [8]
+// Example: ExponentiallySpaced(2,16,2) gives 4 and 8
+// [2] - 3 - (4) - 5 - 6 - 7 - (8) - 9 - 10 - 11 - 12 - 13 - 14 - 15 - [16]
+class ExponentiallySpaced : public UniformlySpaced {
+    public:
+        //Constructor with lower and upper limit and the number of parts to
+        //break the space
+        //Limits are taken care by the factory
+        //We only guarantee that we will not do log operations with zero
+        ExponentiallySpaced (uint_fast64_t min, uint_fast64_t max,
+                uint_fast64_t count_limit) :
+            UniformlySpaced ( (min != 0ul ? min : 1ul), max, count_limit ){  }
+        //Provides the point splitting the next intervals in an exponential scale
+        uint_fast64_t next (  );
+};
 
+// Distribution Generator that only provides the mid point between limits
+// A mid point is the average point in an exponential scale between two limits
+// It behaves similar to an ExponentiallySpaced interval divided into two parts
+// Example: MidPoint(2,8) gives 4
+// [2] - 3 - (4) - 5 - 6 - 7 - [8]
+class MidPoint : public AveragePoint {
+    public:
+        //Usual constructor with lower and upper limit
+        //Limits are taken care by the factory
+        MidPoint (uint_fast64_t min, uint_fast64_t max) :
+            AveragePoint ( (min != 0ul ? min : 1ul), max ){  }
+        //Provides the mid point between lower and upper limits
+        uint_fast64_t next (  );
+};
+
+// Distribution Generator that generates count_limit_ random points.
+// Example: UniformlyRandom(2,8,1) gives anything between 2 and 8 included
+class UniformlyRandom : public DistributionGenerator {
+    private:
+        //Uniform distribution to generate random numbers
+        std::uniform_int_distribution<uint_fast64_t> *randomize;
+        //Generator of random numbers
+        std::random_device device;
+    public:
+        //Constructor with lower and upper limit and the number of random
+        //points to generate
+        //Limits are taken care by the factory
+        UniformlyRandom (uint_fast64_t min, uint_fast64_t max,
+                uint_fast64_t count_limit) {
+            min_ = min;
+            max_ = max;
+            count_limit_ = count_limit;
+            randomize = new std::uniform_int_distribution<uint_fast64_t>(min, max);
+        }
+        //Provides the point splitting the next intervals in an exponential scale
+        uint_fast64_t next (  );
+        //Destructor needs to free random number generators
+        ~UniformlyRandom (  ) {
+            delete randomize;
+        }
+};
+
+
+/****************************************************************************/
 // Method implementations
 
 // Second implementation: can create an Average Point or a Uniformly Spaced
 // distribution.
 DistributionGenerator *DistributionGenerator::make_generator (
-        uint_fast64_t min, uint_fast64_t max, uint_fast64_t count_limit=1lu,
-        Generators generator_kind = AVERAGE_POINT ) {
+        uint_fast64_t min, uint_fast64_t max,
+        Generators generator_kind = AVERAGE_POINT,
+        uint_fast64_t count_limit=1lu ) {
     // First test: min < max -> swaps values to fix it
     if ( min > max ) swap(min, max);
     // Second test: min == max -> increases or decreases one of them
@@ -86,22 +159,61 @@ DistributionGenerator *DistributionGenerator::make_generator (
     // Creates
     if ( generator_kind == AVERAGE_POINT )
         return new AveragePoint(min, max);
-    else
+    else if ( generator_kind == UNIFORMLY_SPACED )
         return new UniformlySpaced(min, max, count_limit);
+    else if ( generator_kind == EXPONENTIALLY_SPACED )
+        return new ExponentiallySpaced(min, max, count_limit);
+    else if ( generator_kind == MID_POINT )
+        return new MidPoint(min, max);
+    else
+        return new UniformlyRandom(min, max, count_limit);
 }
+
 
 // AveragePoint generation operation
 // Returns the average point and counts the call
+// Since we are using 64b numbers, it is not checking for overflows
 uint_fast64_t AveragePoint::next (  ) {
     ++count_;
-    return ( max_ + min_ ) / 2;
+    return ( max_ + min_ ) >> 1;
 }
 
 // UniformlySpaced generation operation
-// Breaks the space in count_limit pieces, returns the n-th (count) point,
-// and counts the calls
+// Breaks the space in count_limit_ (+ 1) pieces, returns the n-th (count) point,
+// and counts the call
 uint_fast64_t UniformlySpaced::next (  ) {
-    uint_fast64_t part = min_ + (count_ * ( max_ - min_ )) / count_limit_;
+    uint_fast64_t point = min_ + ( count_ * ( max_ - min_ )) / count_limit_;
     ++count_;
-    return part;
+    return point;
 }
+
+// ExponentiallySpaced generation operation
+// Breaks the space in count_limit_ (+ 1) pieces, returns the n-th (count) point in
+// an exponential scale
+// and counts the call
+uint_fast64_t ExponentiallySpaced::next (  ) {
+    double log_min = log ( min_ );
+    double log_max = log ( max_ );
+    double point = exp ( log_min + (count_ * ( log_max - log_min ) ) / count_limit_ );
+    ++count_;
+    return (uint_fast64_t) round ( point ); // Handling any rounding errors
+}
+
+// MidPoint generation operation
+// Divides the interval into two parts in an exponential scale
+// and counts the call
+uint_fast64_t MidPoint::next (  ) {
+    double log_min = log ( min_ );
+    double log_max = log ( max_ );
+    double point = exp ( log_min + ( log_max - log_min ) / 2 );
+    ++count_;
+    return (uint_fast64_t) round ( point ); // Handling any rounding errors
+}
+
+// UniformlyRandom generation operation
+// Gets a number in a uniform_int_distribution
+uint_fast64_t UniformlyRandom::next (  ) {
+    ++count_;
+    return (*randomize)(device);
+}
+
--- tests/distribution_generator_test.cpp
+++ tests/distribution_generator_test.cpp
@@ -6,30 +6,31 @@ void test_AveragePoint (  ) {
     DistributionGenerator *generator;
 
     DESCRIBE("Average Point Distribution");
-    WHEN("I create an Average Point from 0 to 10");
-    IFTHEN("I generate a point", "the results should be 5");
-    generator = DistributionGenerator::make_generator(0,10);
-    isEqual(generator->next(), (uint_fast64_t) 5);
 
-    IFTHEN("I generate another point", "it should be 5 again");
-    isEqual(generator->next(), (uint_fast64_t) 5);
+    WHEN("I create an Average Point from 2 to 12");
+    IFTHEN("I generate a point", "the result should be 7");
+    generator = DistributionGenerator::make_generator(2, 12);
+    isEqual(generator->next(), (uint_fast64_t) 7);
+
+    IFTHEN("I generate another point", "it should be 7 again");
+    isEqual(generator->next(), (uint_fast64_t) 7);
 
     IFTHEN("I check if it is done", "it should be done");
     isTrue(generator->is_done());
 
     WHEN("I create another Average Point");
     IFTHEN("I check if it is done", "it should not be done");
-    generator = DistributionGenerator::make_generator(0,10);
+    generator = DistributionGenerator::make_generator(0, 10);
     isFalse(generator->is_done());
 
     WHEN("I create an Average Point from 1000 to 0");
-    IFTHEN("I generate a point", "the results should be 500");
-    generator = DistributionGenerator::make_generator(1000,0);
+    IFTHEN("I generate a point", "the result should be 500");
+    generator = DistributionGenerator::make_generator(1000, 0);
     isEqual(generator->next(), (uint_fast64_t) 500);
 
     WHEN("I create an Average Point with two equal values");
-    IFTHEN("I generate a point", "the results should be equal to the original value due to rounding");
-    generator = DistributionGenerator::make_generator(256,256);
+    IFTHEN("I generate a point", "the result should be equal to the original value due to rounding");
+    generator = DistributionGenerator::make_generator(256, 256);
     isEqual(generator->next(), (uint_fast64_t) 256);
 
 }
@@ -38,29 +39,135 @@ void test_UniformlySpaced (  ) {
     DistributionGenerator *generator;
 
     DESCRIBE("Uniformly Spaced");
-    WHEN("I create a Uniform Distribution from 0 to 100 with 10 points");
-    IFTHEN("I generate a point", "the results should be 0");
-    generator = DistributionGenerator::make_generator(0, 100, 10, UNIFORMLY_SPACED);
-    isEqual(generator->next(), (uint_fast64_t) 0);
-    IFTHEN("I generate a second point", "the results should be 10");
-    isEqual(generator->next(), (uint_fast64_t) 10);
 
-    WHEN("I create another Uniform Distribution with 5 points");
-    IFTHEN("I generate all points", "it should stop after 5 requests");
-    generator = DistributionGenerator::make_generator(0, 100, 5, UNIFORMLY_SPACED);
+    WHEN("I create an Uniform Distribution from 2 to 12 with 1 point (2 intervals)");
+    IFTHEN("I generate a point", "the result should be 7");
+    generator = DistributionGenerator::make_generator(2, 12, UNIFORMLY_SPACED, 1);
+    isEqual(generator->next(), (uint_fast64_t) 7);
+
+    WHEN("I create a Uniform Distribution from 0 to 100 with 9 points (10 intervals)");
+    IFTHEN("I generate a point", "the result should be 10");
+    generator = DistributionGenerator::make_generator(0, 100, UNIFORMLY_SPACED, 9);
+    isEqual(generator->next(), (uint_fast64_t) 10);
+    IFTHEN("I generate a second point", "the result should be 20");
+    isEqual(generator->next(), (uint_fast64_t) 20);
+
+    WHEN("I create another Uniform Distribution with 4 points (5 intervals)");
+    IFTHEN("I generate all points", "it should stop after 4 requests");
+    generator = DistributionGenerator::make_generator(0, 100, UNIFORMLY_SPACED, 4);
     int i = 0;
     uint_fast64_t result;
     while ( ! generator->is_done() ) {
         result = generator->next();
         ++i;
     }
-    isEqual(i, 5);
+    isEqual(i, 4);
     IFTHEN("I generated all points", "the last one should be equal to 80");
     isEqual(result, (uint_fast64_t) 80);
 
+    WHEN("I create a Uniform Distribution from 1 to 2^20 with 10000 points");
+    IFTHEN("I generate points", "they should never be smaller than their previous point");
+    generator = DistributionGenerator::make_generator(1, 1024*1024, UNIFORMLY_SPACED, 10000);
+    bool not_smaller = true;
+    uint_fast64_t previous = 0ul;
+    uint_fast64_t current;
+    while ( (! generator->is_done()) && not_smaller ) {
+        current = generator->next();
+        not_smaller = previous <= current;
+        previous = current;
+    }
+    isTrue(not_smaller);
 }
+
+void test_ExponentiallySpaced (  ) {
+    DistributionGenerator *generator;
+
+    DESCRIBE("Exponentially Spaced");
+
+    WHEN("I create an Exponential Distribution from 2 to 2048 with 9 points (10 intervals)");
+    IFTHEN("I generate a point", "the result should be 4");
+    generator = DistributionGenerator::make_generator(2, 2048, EXPONENTIALLY_SPACED, 9);
+    isEqual(generator->next(), (uint_fast64_t) 4);
+    IFTHEN("I generate the next point", "the result should be 8");
+    isEqual(generator->next(), (uint_fast64_t) 8);
+    IFTHEN("I generate the next point", "the result should be 16");
+    isEqual(generator->next(), (uint_fast64_t) 16);
+
+    WHEN("I create another Exponential Distribution with 4 points (5 intervals)");
+    IFTHEN("I generate all points", "it should stop after 4 requests");
+    generator = DistributionGenerator::make_generator(0, 100, EXPONENTIALLY_SPACED, 4);
+    int i = 0;
+    uint_fast64_t result;
+    while ( ! generator->is_done() ) {
+        result = generator->next();
+        ++i;
+    }
+    isEqual(i, 4);
+
+    WHEN("I create an Exponential Distribution from 1 to 2^20 with 10000 points");
+    IFTHEN("I generate points", "they should never be smaller than their previous point");
+    generator = DistributionGenerator::make_generator(1, 1024*1024, EXPONENTIALLY_SPACED, 10000);
+    bool not_smaller = true;
+    uint_fast64_t previous = 0ul;
+    uint_fast64_t current;
+    while ( (! generator->is_done()) && not_smaller ) {
+        current = generator->next();
+        not_smaller = previous <= current;
+        previous = current;
+    }
+    isTrue(not_smaller);
+}
+
+void test_MidPoint (  ) {
+    DistributionGenerator *generator;
+
+    DESCRIBE("Mid Point Distribution");
+
+    WHEN("I create a Mid Point from 2 to 32");
+    IFTHEN("I generate a point", "the result should be 8");
+    generator = DistributionGenerator::make_generator(2, 32, MID_POINT);
+    isEqual(generator->next(), (uint_fast64_t) 8);
+
+    IFTHEN("I generate another point", "it should be 8 again");
+    isEqual(generator->next(), (uint_fast64_t) 8);
+
+    IFTHEN("I check if it is done", "it should be done");
+    isTrue(generator->is_done());
+
+    WHEN("I create another Mid Point");
+    IFTHEN("I check if it is done", "it should not be done");
+    generator = DistributionGenerator::make_generator(1, 10, MID_POINT);
+    isFalse(generator->is_done());
+
+}
+
+
+void test_UniformlyRandom (  ) {
+    DistributionGenerator *generator;
+
+    DESCRIBE("Uniformly Random");
+
+    WHEN("I create a Random Uniform Distribution from 10 to 1000 with 100 points");
+    IFTHEN("I generate all points", "they should all be between 10 and 1000");
+    generator = DistributionGenerator::make_generator(10, 1000, UNIFORMLY_RANDOM, 100);
+    bool inside_interval = true;
+    uint_fast64_t point = 0ul;
+    int i = 0;
+    while ( (! generator->is_done()) && inside_interval ) {
+        point = generator->next();
+        ++i;
+        inside_interval = ( point >= 10ul ) && ( point <= 1000ul );
+    }
+    isTrue(inside_interval);
+    IFTHEN("I generate all points", "it should stop after 100 requests");
+    isEqual(i, 100);
+}
+
 
 int main () {
     test_AveragePoint();
     test_UniformlySpaced();
+    test_ExponentiallySpaced();
+    test_MidPoint();
+    test_UniformlyRandom();
 }
